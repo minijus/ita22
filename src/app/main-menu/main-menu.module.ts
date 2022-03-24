3edad68a0aee9dc0ac6998cbbfda608107e5af9a
minijus <3633549+minijus@users.noreply.github.com>
2022-03-24 10:37:52 +0200

    feat: add SCAM for main-menu

---
+++ src/app/main-menu/main-menu.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MainMenuComponent } from './main-menu.component';
+
+@NgModule({
+  declarations: [MainMenuComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [MainMenuComponent],
+})
+export class MainMenuModule {}
